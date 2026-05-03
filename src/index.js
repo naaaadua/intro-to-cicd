c4502c93109a97cdf78c622f95959f45f3b7898a
naaaadua <sanang@afreximbank.com>
2026-05-03 18:04:19 +0300

    added a comment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// This is the main entry point of the application. It exports a function that takes a name as an argument and returns a greeting message.
 function sayHi(name) {
   return `Hello there ${name}`
 }
